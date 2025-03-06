b8999da69b03c843e4c094ea5033f97a4026030e
Erika Silva dos Santos <erika.s.santos16@gmail.com>
2025-03-06 19:53:28 -0300

    manutencao

---
+++ 1desafio/logica-js-projeto_inicial/app.js
@@ -0,0 +1,10 @@
+alert('Bem-vindo ao nosso site!')
+let nome = prompt('Digite seu nome:')
+let idade = prompt('Digite sua idade:')
+
+if (idade >= 18){
+    prompt (nome + ', você é maior de idade, já podemos iniciar o processo para sua habilitação. Boa sorte!')
+}
+ else {
+    prompt (nome + ', você é menor de idade, por isso ainda não podemos iniciar o processo para sua habilitação')
+ }
---
+++ 2Desafio/js-curso-2-desafio_1/app.js
@@ -0,0 +1,15 @@
+let titulo = document.querySelector ('h1');
+titulo.innerHTML = 'Hora do desafio';
+
+function botaoConsole (){
+    console.log ('O botão foi clicado')
+};
+
+function botaoAlerta (){
+   alert('Eu amo JS')
+};
+
+function cidade(){
+  let nomeCidade = prompt('Digite a cidade que visitou:')
+        alert('Eu estive lá lembrei de você') 
+    }
